00767fa564dcc83ac0dc173cb4ef91b64cd9fc3a
lingeswaran <717823s132@kce.ac.in>
2026-06-10 11:24:54 +0530

    re  updated services 11.24 am on 10-06-2026

--- Backend/shipment/shipment/src/main/java/com/shipfast/shipment/controller/ShipmentController.java
+++ Backend/shipment/shipment/src/main/java/com/shipfast/shipment/controller/ShipmentController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -33,7 +32,6 @@ import com.shipfast.shipment.service.ShipmentService;
 
 @RestController
 @RequestMapping("/api/v1/shipments")
-
 public class ShipmentController {
 
     private final ShipmentService shipmentService;
